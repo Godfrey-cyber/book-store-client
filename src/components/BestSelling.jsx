92943341f762bc5c290b57729f8b715646e36948
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-02-02 11:05:13 +0300

    fixing-morebooks-comp

--- src/components/BestSelling.jsx
+++ src/components/BestSelling.jsx
@@ -52,9 +52,9 @@ const BestSelling = () => {
 		getBooks()
 	}, [])
 	return (
-		<section className="flex flex-col w-full bg-red-50">
+		<section className="flex flex-col w-full bg-red-50 w-full">
 			{/*title*/}
-			<p className="text-lg font-semibold md:text-xl lg:text-3xl w-full text-gray-700 text-center py-8 md:py-12 lg:py-16">Best Selling Books Ever</p>
+			<p className="text-lg font-semibold md:text-xl lg:text-3xl w-full text-red-700 text-center py-8 md:py-12 lg:py-16">Best Selling Books Ever</p>
 			{/*//NOVELS*/}
 			<div className="best_book">
 				{books && books.slice(0, 5).map(book => (
