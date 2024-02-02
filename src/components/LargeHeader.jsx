ab28d51e0b8639afcf4c9f5312848e89a22aa3e7
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-02-02 13:55:36 +0300

    search-bar

--- src/components/LargeHeader.jsx
+++ src/components/LargeHeader.jsx
@@ -60,44 +60,46 @@ const LargeHeader = () => {
 	
 	return (
 		<nav className={`navbar ${scrolled ? 'scrolled' : ''} large_header`}>
-        	<div className="flex space-x-3 items-center">
-	        	<span onClick={() => navigate("/")} className="flex h-8 cursor-pointer w-auto">
-	        		<img className="w-full h-full" src="https://preview.colorlib.com/theme/abcbook/assets/img/logo/logo.png" />
-	        	</span>
-        	</div>
-	        {/*SEARCH*/}
-	        <div className="flex flex-col relative">
-		        <div className="search_div">
-		        	<input value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} type="text" className="search_input" placeholder="Search book by author, title or publisher" />
-		        	<FaSearch className="search_icon" />
-		        </div>
-		        {searchTerm && <div className="largeheader_div">
-		        	<span className="largeheader_span">
-		        		<span className="flex text-sm font-normal text-gray-800">Search results for <p className="text-sm font-semibold text-gray-800 ml-2">"{searchTerm}"</p></span>
+			<span className="flex justify-between items-center">
+	        	<div className="flex space-x-3 items-center">
+		        	<span onClick={() => navigate("/")} className="flex h-8 cursor-pointer w-auto">
+		        		<img className="w-full h-full" src="https://preview.colorlib.com/theme/abcbook/assets/img/logo/logo.png" />
 		        	</span>
-		        	{!loading ? searchResults && searchResults.map(book => (
-		        			<div key={book._id} onClick={() => navigate(`/book_details/${book._id}`)} className="group flex space-x-3 cursor-pointer hover:bg-gray-100">
-		        				<img src={book.photo} className="h-24 w-16 rounded" alt="" />
-		        				<span className="flex flex-col justify-between p-2">
-		        					<p className="text-sm font-semibold text-gray-800 group-hover:text-red-400 transition-all delay-200">{book.title}</p>
-		        					<p className="text-xs font-normal text-gray-700">by {book.author}</p>
-		        				</span>
-		        			</div>
-		        		)) : <p className="">loading</p>}
+	        	</div>
+		        {/*SEARCH*/}
+		        <div className="flex-col relative hidden lg:flex">
+			        <div className="search_div">
+			        	<input value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} type="text" className="search_input" placeholder="Search book by author, title or publisher" />
+			        	<FaSearch className="search_icon" />
+			        </div>
+			        {searchTerm && <div className="largeheader_div">
+			        	<span className="largeheader_span">
+			        		<span className="flex text-sm font-normal text-gray-800">Search results for <p className="text-sm font-semibold text-gray-800 ml-2">"{searchTerm}"</p></span>
+			        	</span>
+			        	{!loading ? searchResults && searchResults.map(book => (
+			        			<div key={book._id} onClick={() => navigate(`/book_details/${book._id}`)} className="group flex space-x-3 cursor-pointer hover:bg-gray-100">
+			        				<img src={book.photo} className="h-24 w-16 rounded" alt="" />
+			        				<span className="flex flex-col justify-between p-2">
+			        					<p className="text-sm font-semibold text-gray-800 group-hover:text-red-400 transition-all delay-200">{book.title}</p>
+			        					<p className="text-xs font-normal text-gray-700">by {book.author}</p>
+			        				</span>
+			        			</div>
+			        		)) : <p className="">loading...</p>}
+			        </div>
+			    }
 		        </div>
-		    }
-	        </div>
-	        {/*CTA'S*/}
-	        <div className="flex space-x-4 items-center">
-	        	<p className="cta_p">FAQ</p>
-	        	<p className="cta_p">Track Order</p>
-	        	<span onClick={() => navigate("/cart_page")} className="cursor-pointer">
-	        		<BsCart className="cta_icon" />
-	        		<span className="cta_span">{ items > 9 ? "9+" : items }</span>
-	        	</span>
-	        	{!user ? <button onClick={() => navigate("/sign_up")} type="button" className="signin_btn">Log In</button> : 
-	        	<button onClick={() => logoutUser(dispatch)} type="button" className="signin_btn">Log Out</button>}
-	        </div>
+		        {/*CTA'S*/}
+		        <div className="flex space-x-4 items-center">
+		        	<p className="cta_p">FAQ</p>
+		        	<p className="cta_p">Track Order</p>
+		        	<span onClick={() => navigate("/cart_page")} className="cursor-pointer">
+		        		<BsCart className="cta_icon" />
+		        		<span className="cta_span">{ items > 9 ? "9+" : items }</span>
+		        	</span>
+		        	{!user ? <button onClick={() => navigate("/sign_up")} type="button" className="signin_btn">Log In</button> : 
+		        	<button onClick={() => logoutUser(dispatch)} type="button" className="signin_btn">Log Out</button>}
+		        </div>
+		    </span>
         </nav>
 	)
 }
