f33965bececbff6600a87c34a14d3dadc968b96c
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-02-05 18:00:23 +0300

    book-page rating

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -178,7 +178,7 @@ const BookPage = () => {
 					<img className="rounded-full h-12 w-12 bg-cover" src={book.photo} alt={book.photo} />
 					<p className="text-gray-700 font-normal text-2xl lg:text-3xl">What do you think?</p>
 					<div className="flex-col lg:flex space-x-5 items-center">
-						<div className="flex-col space-y-3">
+						<div className="flex-col space-y-3 my-3 items-center justify-center">
 							<span className="flex items-center cursor-pointer">
 								<FaStar className="text-2xl lg:text-3xl text-red-500" />
 								<FaStar className="text-2xl lg:text-3xl text-red-500" />
