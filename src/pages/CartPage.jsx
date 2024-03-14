848c9b6ef5eb7b6bce0239ec1da36447093dd8a9
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-03-14 15:36:02 +0300

    cart-page-bug-fix-1

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -63,7 +63,7 @@ const CartPage = () => {
 									<div className="h-44 w-32 flex-col">
 										<img onClick={() => navigate(`/book_details/${book._id}`)} className="h-full w-full bg-contain" src={book.photo} alt="" />
 									</div>
-									<div className="flex flex-col justify-between">
+									<div className="flex-col justify-between">
 										<p className="text-sm font-light text-gray-700 truncate">{book.title}</p>
 										<p className="text-lg font-light text-red-400 slashed-zero">KSH: {book.price * book.count}</p>
 									</div>
