6c2aaedcd1c60ea0ec3a533e887aa3d798bb98e7
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-03-15 11:28:56 +0300

    hello-cart-bug

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -64,7 +64,7 @@ const CartPage = () => {
 										<img onClick={() => navigate(`/book_details/${book._id}`)} className="h-full w-full bg-contain" src={book.photo} alt={book.title} />
 									</div>
 									<div className="flex col-span-8 flex-col justify-between h-full bg-blue-400">
-										<p className="text-sm line-clamp-3 font-light text-gray-700 truncate">{book.title}</p>
+										<p className="text-sm w-44 font-light text-gray-700 truncate">{book.title}</p>
 										<p className="text-lg font-light text-red-400 slashed-zero">KSH: {book.price * book.count}</p>
 									</div>
 								</div>
