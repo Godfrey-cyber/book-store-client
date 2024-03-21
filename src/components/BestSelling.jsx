e6a4bb2dca9dde595ab07822c7578c537b4a5e2a
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-03-21 13:50:58 +0300

    footer-comp

--- src/components/BestSelling.jsx
+++ src/components/BestSelling.jsx
@@ -55,26 +55,6 @@ const BestSelling = () => {
 	return (
 		<section className="flex flex-col bg-red-50">
 			<p className="best_selling_title">Best Selling Books Ever</p>
-			{/*<div className="best_book">
-				<div onClick={() => navigate(`/book_details/${book._id}`)} key={book._id} className="best_div group">
-					<img className="best_img group-hover:rounded-t-md" src={book.photo} alt="" />
-					<div className="flex flex-col space-y-.5 px-2">
-						<p className="text-xs md:text-sm font-normal md:font-medium text-gray-700 truncate">{book?.title.length > 19 ? book.title?.slice(0, 19)+ '...' : book.title}</p>
-						<p className="text-xs md:text-sm font-light text-gray-500 truncate">{!book.author ? "J. R Rain" : book.author.length > 18 ? book.author.slice(0, 18)+ '...' : book.author}</p>
-						<span className="flex items-center hidden lg:flex">
-							<FaStar className="best_icons" />
-							<FaStar className="best_icons" />
-							<FaStar className="best_icons" />
-							<FaStar className="best_icons" />
-							<FaStarHalfStroke className="text-sm text-red-500" />
-						</span>
-						<span className="flex items-center justify-between">
-							<p className="text-xs font-light text-gray-700">(120 Review)</p>
-							<p className="text-xs font-light lg:text-sm lg:font-bold text-red-500">Ksh. {book.price}</p>
-						</span>
-					</div>
-				</div>
-			</div>*/}
 			<div className="best_book">
 				{books && books.slice(0, 5).map(book => (
 					<Book key={book._id} book={book} />
