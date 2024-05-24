bb5e4089f065600dfb0606968e195b2c63734bd2
Godfrey Ndiritu <godfreyndiritu26@gmail.com>
2024-05-24 23:14:09 +0300

    test-commit

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -13,8 +13,8 @@ const Book = ({ book }) => {
 				<MdAddShoppingCart className="text-lg ml-1" />
 			</span>q*/}
 			<div className="flex flex-col space-y-.5 px-2">
-				<p className="text-xs md:text-sm font-normal md:font-medium text-gray-700 truncate">{book?.title.length > 19 ? book.title?.slice(0, 19)+ '...' : book.title}</p>
-				<p className="text-xs md:text-sm font-light text-gray-500 truncate">{!book.author ? "J. R Rain" : book.author.length > 18 ? book.author.slice(0, 18)+ '...' : book.author}</p>
+				<p className="text-sm md:text-lg font-normal md:font-medium text-gray-700 truncate">{book?.title.length > 19 ? book.title?.slice(0, 19)+ '...' : book.title}</p>
+				<p className="text-sm md:text-lg font-light text-gray-500 truncate">{!book.author ? "J. R Rain" : book.author.length > 18 ? book.author.slice(0, 18)+ '...' : book.author}</p>
 				<span className="flex items-center hidden lg:flex">
 					<FaStar className="best_icons" />
 					<FaStar className="best_icons" />
@@ -23,8 +23,8 @@ const Book = ({ book }) => {
 					<FaStarHalfStroke className="text-sm text-red-500" />
 				</span>
 				<span className="flex items-center justify-between">
-					<p className="text-xs font-light text-gray-700">(120 Review)</p>
-					<p className="text-xs font-light lg:text-sm lg:font-bold text-red-500">Ksh. {book.price}</p>
+					<p className="text-sm md:text-lg text-gray-700">(120 Review)</p>
+					<p className="text-sm md:text-lg lg:font-bold text-red-500">Ksh. {book.price}</p>
 				</span>
 			</div>
 		</div>
